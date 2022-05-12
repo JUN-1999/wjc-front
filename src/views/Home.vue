c43e02ef61b0c28cd369c73a1bcbd4ee0e5d1537
root <1173943332@qq.com>
2022-05-12 12:11:04 +0800

    guagua

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    
+    <div>
+        <a href="https://v.youku.com/v_show/id_XMzU5MjIyODY1Mg==.html?spm=a2h0c.8166622.PhoneSokuProgram_1.dselectbutton_1&showid=09efbfbd55efbfbd4fef">请在没人的时候打开</a>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+a{
+    font-size:50px;
+    font-weight: 900;
+}
+
+</style>
